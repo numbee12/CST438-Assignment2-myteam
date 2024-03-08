8d5466e3312cb8a18405499e78be5cf211d84066
Robin Hurtado <rohurtado@csumb.edu>
2024-03-08 15:03:34 -0800

    completed grade class

--- src/main/java/com/cst438/domain/Grade.java
+++ src/main/java/com/cst438/domain/Grade.java
@@ -11,7 +11,49 @@ public class Grade {
  
     // TODO complete this class
     // add additional attribute for score
+    private int score;
+
     // add relationship between grade and assignment entities
+    @ManyToOne
+    @JoinColumn(name="assignment_id", nullable = false)
+    private Assignment assignment;
+
     // add relationship between grade and enrollment entities
+    @ManyToOne
+    @JoinColumn(name = "enrollment_id", nullable = false)
+    private Enrollment enrollment;
+
     // add getter/setter methods
+
+    public int getGradeId() {
+        return gradeId;
+    }
+
+    public void setGradeId(int gradeId) {
+        this.gradeId = gradeId;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public Assignment getAssignment() {
+        return assignment;
+    }
+
+    public void setAssignment(Assignment assignment) {
+        this.assignment = assignment;
+    }
+
+    public Enrollment getEnrollment() {
+        return enrollment;
+    }
+
+    public void setEnrollment(Enrollment enrollment) {
+        this.enrollment = enrollment;
+    }
 }
