750eb54c3a985c8fc1ee259d53cd6647d1a63b43
Juan Duarte <juduarte@csumb.edu>
2024-03-10 22:46:08 -0700

    Converted dueDate from String to SQL.DATE

--- src/main/java/com/cst438/domain/Assignment.java
+++ src/main/java/com/cst438/domain/Assignment.java
@@ -3,6 +3,7 @@ package com.cst438.domain;
 import jakarta.persistence.*;
 
 import java.sql.Date;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 @Entity
@@ -14,7 +15,7 @@ public class Assignment {
 
     // add additional attributes for title, dueDate
     private String title;
-    private String dueDate;
+    private Date dueDate;
 
     // add relationship between assignment and section entities
     @ManyToOne
@@ -26,9 +27,17 @@ public class Assignment {
     public void setAssignmentId(int assignmentId){this.assignmentId= assignmentId;}
     public String getTitle(){return title;}
     public void setTitle(String title){this.title = title;}
-    public String getDueDate(){return dueDate;}
-    public void setDueDate(String dueDate){this.dueDate= dueDate;}
-
+    public String getDueDate(){
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        return dateFormat.format(dueDate);}
+    public void setDueDate(String dateString) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+           dueDate = new Date(dateFormat.parse(dateString).getTime());
+        } catch (ParseException e) {
+            System.err.println("Error parsing dueDate: " + e.getMessage());
+        }
+    }
     public Section getSection(){return section;}
     public void setSection(Section section){this.section= section;}
 }
--- src/main/java/com/cst438/domain/AssignmentRepository.java
+++ src/main/java/com/cst438/domain/AssignmentRepository.java
@@ -9,7 +9,7 @@ public interface AssignmentRepository extends CrudRepository<Assignment, Integer
 
     // TODO uncomment the following lines as needed
 
-    @Query("select a from Assignment a where a.section.sectionNo=:sectionNo")
+    @Query("select a from Assignment a where a.section.sectionNo=:sectionNo order by a.dueDate")
     List<Assignment> findBySectionNoOrderByDueDate(int sectionNo);
 
     @Query("select a from Assignment a join a.section.enrollments e " +
