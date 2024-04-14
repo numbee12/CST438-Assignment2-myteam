877489d59e8b39be5d51823f3cb827d60d3974e7
Juan Duarte <juduarte@csumb.edu>
2024-04-14 13:55:52 -0700

    Added principal arguments for enrollment

--- src/main/java/com/cst438/controller/EnrollmentController.java
+++ src/main/java/com/cst438/controller/EnrollmentController.java
@@ -3,8 +3,12 @@ package com.cst438.controller;
 
 import com.cst438.domain.Enrollment;
 import com.cst438.domain.EnrollmentRepository;
+import com.cst438.domain.Section;
+import com.cst438.domain.SectionRepository;
 import com.cst438.domain.User;
+import com.cst438.domain.UserRepository;
 import com.cst438.dto.EnrollmentDTO;
+import java.security.Principal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -20,13 +24,26 @@ public class EnrollmentController {
 
     @Autowired
     EnrollmentRepository enrollmentRepository;
+    @Autowired
+    SectionRepository sectionRepository;
+    @Autowired
+    UserRepository userRepository;
 
     // instructor downloads student enrollments for a section, ordered by student name
     // user must be instructor for the section
     @GetMapping("/sections/{sectionNo}/enrollments")
     @PreAuthorize("hasAuthority('SCOPE_ROLE_INSTRUCTOR')")
     public List<EnrollmentDTO> getEnrollments(
-            @PathVariable("sectionNo") int sectionNo ) {
+            @PathVariable("sectionNo") int sectionNo, Principal principal) {
+
+        String instructorEmail = principal.getName();
+
+        Section section = sectionRepository.findById(sectionNo).orElse(null);
+        if (section == null) {
+            throw new ResponseStatusException(HttpStatus.NO_CONTENT, "Section not found "+ sectionNo);
+        } else if (section.getInstructorEmail() == null || !section.getInstructorEmail().equals(instructorEmail)) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Unauthorized User"+ instructorEmail);
+        }
 
         List<Enrollment> enrollmentList = enrollmentRepository.findEnrollmentsBySectionNoOrderByStudentName(sectionNo);
         List<EnrollmentDTO> dto_list = new ArrayList<EnrollmentDTO>();
@@ -56,13 +73,18 @@ public class EnrollmentController {
     // user must be instructor for the section
     @PutMapping("/enrollments")
     @PreAuthorize("hasAuthority('SCOPE_ROLE_INSTRUCTOR')")
-    public void updateEnrollmentGrade(@RequestBody List<EnrollmentDTO> dlist) {
-    
+    public void updateEnrollmentGrade(@RequestBody List<EnrollmentDTO> dlist, Principal principal) {
+        String instructorEmail = principal.getName();
+        User instructor = userRepository.findByEmail(instructorEmail) ;
+
+        if (!instructor.getType().equals("INSTRUCTOR")) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Unauthorized User"+ instructorEmail);
+            }
+
         for (EnrollmentDTO eDTO : dlist) {
             Enrollment e = enrollmentRepository
                             .findById(eDTO.enrollmentId())
                             .orElse(null);
-
             if (e == null) {
                 throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Enrollment not found " + eDTO.enrollmentId());
             }
